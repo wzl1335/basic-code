c745fddd23bbb776a2d60fc3ce23e1ea729846e1
Administrator <2821795990@qq.com>
2021-05-09 15:24:48 +0800

    111

---
+++ days01-code/src/cn/itcast/day05/demo4/Body.java
@@ -0,0 +1,12 @@
+package cn.itcast.day05.demo4;
+
+public class Body {
+    public void mes(){
+        System.out.println("外部类方法");
+    }
+    public class Heart{
+        public void me(){
+            System.out.println("这是一个内部类");
+        }
+    }
+}
